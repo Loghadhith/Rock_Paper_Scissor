2031d2f08d7bacc9ba1d08e1c7d34474d8856595
Loghadhith JR <120036835+Loghadhith@users.noreply.github.com>
2023-05-03 10:24:39 +0530

    Update des.js

--- des.js
+++ des.js
@@ -1,5 +1,11 @@
 //1=rock 2=paper 3=scissor
 let x;
+let cp;
+let cc;
+cp=Number(cp);
+cc=Number(cc);
+cp=0;
+cc=0;
 document.getElementById("Rock").onclick=function(){
     x=Math.ceil(Math.random()*3);
     console.log(x);
@@ -13,11 +19,15 @@ document.getElementById("Rock").onclick=function(){
     {
         document.getElementById("Print").innerHTML="Computer Wins";
         //window.alert("Computer Wins");
+        cc=cc+1;
     }
     else{
         document.getElementById("Print").innerHTML="You Win";
         //window.alert("You Win");
+        cp=cp+1;
     }
+    document.getElementById("cp").innerHTML=cp;
+    document.getElementById("cc").innerHTML=cc;
 }
 document.getElementById("Paper").onclick=function(){
     x=Math.ceil(Math.random()*3);
@@ -27,6 +37,7 @@ document.getElementById("Paper").onclick=function(){
     {
         document.getElementById("Print").innerHTML="You Win";
         //window.alert("You Win");
+        cp=cp+1;
     }
     else if(x==2)
     {
@@ -36,7 +47,10 @@ document.getElementById("Paper").onclick=function(){
     else{
         document.getElementById("Print").innerHTML="Computer Wins";
         //window.alert("Computer Wins");
+        cc=cc+1;
     }
+    document.getElementById("cp").innerHTML=cp;
+    document.getElementById("cc").innerHTML=cc;
 }
 document.getElementById("Scissor").onclick=function(){
     x=Math.ceil(Math.random()*3);
@@ -46,14 +60,18 @@ document.getElementById("Scissor").onclick=function(){
     {
         document.getElementById("Print").innerHTML="Computer Wins";
         //window.alert("Computer Wins");
+        cc=cc+1;
     }
     else if(x==2)
     {
         document.getElementById("Print").innerHTML="You Win";
         //window.alert("You Win");
+        cp=cp+1
     }
     else{
         document.getElementById("Print").innerHTML="It's a Tie";
         //window.alert("It's a Tie");
     }
+    document.getElementById("cp").innerHTML=cp;
+    document.getElementById("cc").innerHTML=cc;
 }
